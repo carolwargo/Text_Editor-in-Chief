59fc452f655571192b52ff890eca6da4c15e43ec
Carol Wargo <Carolwargo@gmail.com>
2023-06-27 21:10:14 -0400

    added caching logic

--- client/src-sw.js
+++ client/src-sw.js
@@ -25,6 +25,16 @@ warmStrategyCache({
 });
 
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
-
 // TODO: Implement asset caching
-registerRoute();
+
+registerRoute(
+  ({ request }) => ['stlye', 'script', 'worker'].includes(request.destination),
+  new StaleWhileRevalidate({
+    cacheName: 'asset-cache',
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+    ],
+  }),
+);
