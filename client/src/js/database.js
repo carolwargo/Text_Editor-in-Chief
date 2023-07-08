5ebd22c6706e40d2d1cb8ed125eefa6462bc872b
Carol Wargo <Carolwargo@gmail.com>
2023-07-08 10:32:32 -0400

    updated db code

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -17,7 +17,7 @@ export const putDb = async (id, content) => {
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  const request = store.put({ id: 1, jate: content });
+  const request = store.put({ id, jate: content });
   const result = await request;
   console.log('🚀 - data saved to the database', result);
 };
